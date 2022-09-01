e29faaaf93c6c579cc94aaf68ddf766529b854d8
Mathieu Livebardon <tsunoll96@gmail.com>
2022-09-01 17:15:56 +0100

    Focus on city object button

--- test/functional/CameraUtils.js
+++ test/functional/CameraUtils.js
@@ -9,20 +9,22 @@ describe('Camera utils with globe example', function _() {
             const raycaster = new THREE.Raycaster();
             const screen = new THREE.Vector2();
             const ellipsoid = new itowns.Ellipsoid(itowns.ellipsoidSizes);
-            view
-                .getPickingPositionFromDepth = function fn(mouse, target = new THREE.Vector3()) {
-                    const g = this.mainLoop.gfxEngine;
-                    const dim = g.getWindowSize();
-                    screen.copy(mouse || dim.clone().multiplyScalar(0.5));
-                    screen.x = Math.floor(screen.x);
-                    screen.y = Math.floor(screen.y);
-                    screen.x = ((screen.x / dim.x) * 2) - 1;
-                    screen.y = (-(screen.y / dim.y) * 2) + 1;
-                    raycaster.setFromCamera(screen, this.camera.camera3D);
-                    target.copy(ellipsoid.intersection(raycaster.ray));
+            view.getPickingPositionFromDepth = function fn(
+                mouse,
+                target = new THREE.Vector3(),
+            ) {
+                const g = this.mainLoop.gfxEngine;
+                const dim = g.getWindowSize();
+                screen.copy(mouse || dim.clone().multiplyScalar(0.5));
+                screen.x = Math.floor(screen.x);
+                screen.y = Math.floor(screen.y);
+                screen.x = (screen.x / dim.x) * 2 - 1;
+                screen.y = -(screen.y / dim.y) * 2 + 1;
+                raycaster.setFromCamera(screen, this.camera.camera3D);
+                target.copy(ellipsoid.intersection(raycaster.ray));
 
-                    return target;
-                };
+                return target;
+            };
         });
     });
 
@@ -31,8 +33,10 @@ describe('Camera utils with globe example', function _() {
         const result = await page.evaluate((p) => {
             const camera = view.camera.camera3D;
             return itowns.CameraUtils.transformCameraToLookAtTarget(
-                view, camera, p,
-            ).then(final => final.range);
+                view,
+                camera,
+                p,
+            ).then((final) => final.range);
         }, params);
 
         assert.ok(Math.abs(result - params.range) / params.range < 0.05);
@@ -40,11 +44,18 @@ describe('Camera utils with globe example', function _() {
     it('should look at coordinate like expected', async () => {
         const params = { longitude: 60, latitude: 40 };
         const result = await page.evaluate((p) => {
-            const coord = new itowns.Coordinates('EPSG:4326', p.longitude, p.latitude, 0);
+            const coord = new itowns.Coordinates(
+                'EPSG:4326',
+                p.longitude,
+                p.latitude,
+                0,
+            );
             const camera = view.camera.camera3D;
             return itowns.CameraUtils.transformCameraToLookAtTarget(
-                view, camera, { coord },
-            ).then(final => final.coord);
+                view,
+                camera,
+                { coord },
+            ).then((final) => final.coord);
         }, params);
         assert.equal(Math.round(result.x), params.longitude);
         assert.equal(Math.round(result.y), params.latitude);
@@ -55,8 +66,10 @@ describe('Camera utils with globe example', function _() {
         const result = await page.evaluate((p) => {
             const camera = view.camera.camera3D;
             return itowns.CameraUtils.transformCameraToLookAtTarget(
-                view, camera, p,
-            ).then(final => final.tilt);
+                view,
+                camera,
+                p,
+            ).then((final) => final.tilt);
         }, params);
         assert.equal(Math.round(result), params.tilt);
     });
@@ -66,8 +79,10 @@ describe('Camera utils with globe example', function _() {
         const result = await page.evaluate((p) => {
             const camera = view.camera.camera3D;
             return itowns.CameraUtils.transformCameraToLookAtTarget(
-                view, camera, p,
-            ).then(final => final.heading);
+                view,
+                camera,
+                p,
+            ).then((final) => final.heading);
         }, params);
         assert.equal(Math.round(result), params.heading);
     });
@@ -84,14 +99,19 @@ describe('Camera utils with globe example', function _() {
                 coord: new itowns.Coordinates('EPSG:4326', 3, 47, 0),
             };
             return itowns.CameraUtils.transformCameraToLookAtTarget(
-                view, camera, params,
-            ).then(final => ({ params, final }));
+                view,
+                camera,
+                params,
+            ).then((final) => ({ params, final }));
         });
         assert.equal(Math.round(result.final.heading), result.params.heading);
         assert.equal(Math.round(result.final.tilt), result.params.tilt);
         assert.equal(Math.round(result.final.coord.x), result.params.coord.x);
         assert.equal(Math.round(result.final.coord.y), result.params.coord.y);
-        assert.equal(Math.round(result.final.range / 10000) * 10000, result.params.range);
+        assert.equal(
+            Math.round(result.final.range / 10000) * 10000,
+            result.params.range,
+        );
     });
 
     it('should heading, tilt, range and coordinate like expected with animation (500ms)', async () => {
@@ -106,14 +126,19 @@ describe('Camera utils with globe example', function _() {
                 time: 500,
             };
             const camera = view.camera.camera3D;
-            return itowns.CameraUtils
-                .animateCameraToLookAtTarget(view, camera, params)
-                .then(final => ({ final, params }));
+            return itowns.CameraUtils.animateCameraToLookAtTarget(
+                view,
+                camera,
+                params,
+            ).then((final) => ({ final, params }));
         });
         assert.equal(Math.round(result.final.heading), result.params.heading);
         assert.equal(Math.round(result.final.tilt), result.params.tilt);
         assert.equal(Math.round(result.final.coord.x), result.params.coord.x);
         assert.equal(Math.round(result.final.coord.y), result.params.coord.y);
-        assert.equal(Math.round(result.final.range / 1000) * 1000, result.params.range);
+        assert.equal(
+            Math.round(result.final.range / 1000) * 1000,
+            result.params.range,
+        );
     });
 });
