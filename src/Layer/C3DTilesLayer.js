9c503a16416fd8a24d5e663e2c4971ad88ae3d91
Mathieu Livebardon <tsunoll96@gmail.com>
2022-09-15 15:56:59 +0100

    useless changes format C3Dtileslayer

--- src/Layer/C3DTilesLayer.js
+++ src/Layer/C3DTilesLayer.js
@@ -104,30 +104,19 @@ class C3DTilesLayer extends GeometryLayer {
                         )
                     ) {
                         // if it is required to load the tileset
-                        if (
-                            this.tileset.extensionsRequired &&
-                            this.tileset.extensionsRequired.includes(
-                                extensionUsed,
-                            )
-                        ) {
+                        if (this.tileset.extensionsRequired &&
+                            this.tileset.extensionsRequired.includes(extensionUsed)) {
                             console.error(
-                                `3D Tiles tileset required extension "${extensionUsed}" must be registered to the 3D Tiles layer of iTowns to be parsed and used.`,
-                            );
+                                `3D Tiles tileset used extension "${extensionUsed}" must be registered to the 3D Tiles layer of iTowns to be parsed and used.`);
                         } else {
                             console.warn(
-                                `3D Tiles tileset used extension "${extensionUsed}" must be registered to the 3D Tiles layer of iTowns to be parsed and used.`,
-                            );
+                                `3D Tiles tileset used extension "${extensionUsed}" must be registered to the 3D Tiles layer of iTowns to be parsed and used.`);
                         }
                     }
                 }
             }
             // TODO: Move all init3dTilesLayer code to constructor
-            init3dTilesLayer(
-                view,
-                view.mainLoop.scheduler,
-                this,
-                tileset.root,
-            ).then(resolve);
+            init3dTilesLayer(view, view.mainLoop.scheduler, this, tileset.root).then(resolve);
         });
     }
 
@@ -138,6 +127,7 @@ class C3DTilesLayer extends GeometryLayer {
     update(context, layer, node) {
         return update(context, layer, node);
     }
+
     generateCityObjects(tile) {
         tile.cityObjectManager = new C3DTCityObjectManager();
 
