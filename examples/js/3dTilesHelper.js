296e1049bb5194754346af3c601fcaf588b8e374
Mathieu Livebardon <tsunoll96@gmail.com>
2022-09-15 15:35:30 +0100

    move code in src

--- examples/js/3dTilesHelper.js
+++ examples/js/3dTilesHelper.js
@@ -7,10 +7,8 @@
 // eslint-disable-next-line
 function fillHTMLWithPickingInfo(event, pickingArg) {
     if (!pickingArg.layer.isC3DTilesLayer) {
-        console.warn(
-            "Function fillHTMLWithPickingInfo only works" +
-                " for C3DTilesLayer layers."
-        );
+        console.warn('Function fillHTMLWithPickingInfo only works' +
+            ' for C3DTilesLayer layers.');
         return;
     }
 
@@ -21,62 +19,14 @@ function fillHTMLWithPickingInfo(event, pickingArg) {
 
     // Get intersected objects
     var intersects = pickingArg.view.pickObjectsAt(event, 5, pickingArg.layer);
-    if (intersects.length === 0) {
-        return;
-    }
+    if (intersects.length === 0) { return; }
 
     // Get information from intersected objects (from the batch table and
     // eventually the 3D Tiles extensions
-    var featureDisplayableInfo =
-        pickingArg.layer.getInfoFromIntersectObject(intersects);
+    var featureDisplayableInfo = pickingArg.layer.getInfoFromIntersectObject(intersects);
+
     if (featureDisplayableInfo) {
         // eslint-disable-next-line
-        pickingArg.htmlDiv.appendChild(
-            createHTMLListFromObject(featureDisplayableInfo)
-        );
-        featureDisplayableInfo.tile = getTileFromObjectIntersected(
-            intersects[0].object
-        );
-        return featureDisplayableInfo;
+        pickingArg.htmlDiv.appendChild(createHTMLListFromObject(featureDisplayableInfo));
     }
 }
-
-/**
- * It takes an object and returns the tile that contains it
- * @param object - The intersected object.
- * @returns The tile.
- */
-function getTileFromObjectIntersected(object) {
-    if (!object) {
-        throw "Tile not loaded in view";
-    }
-
-    //Find the 'Object3D' part of the tile
-    while (!!object.parent && !(object.type === "Object3D")) {
-        object = object.parent;
-    }
-
-    if (!object.batchTable) {
-        throw "Invalid tile : no batch table";
-    }
-
-    return object;
-}
-
-/**
- * Return the first intersected object that is visible and has a visible tile and a visible tile
- * content.
- * @param intersects - the array of intersections returned by the raycaster
- * @returns The first intersection that is visible.
- */
-function getFirstIntersection(intersects) {
-    for (let inter of intersects) {
-        let tile = getTileFromObjectIntersected(inter.object);
-        if (inter.object.visible && tile.visible && tile.content.visible) {
-            return inter;
-        }
-    }
-    return null;
-}
-
-export { getTileFromObjectIntersected, getFirstIntersection };
